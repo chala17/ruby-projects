8ae8f737fe213eb9bcfa44f56baecb109227e879
chala17 <donathin17@gmail.com>
2020-10-10 19:29:33 -0700

    completed all specs in connect_four_specs, completed all logic for connect_four.rb other than diagonal? and horizontal?

--- connect_four/lib/connect_four.rb
+++ connect_four/lib/connect_four.rb
@@ -10,9 +10,22 @@ class Player
   end
 
   def make_a_move(board)
-    puts "Enter which column you'd like to drop your token in Player#{self.player}"
-    move = gets.chomp.to_i until !move.nil? && move >= 0 && move < 8
-    board.spaces[move].push(self.symbol)
+    accepted_move = false
+    until accepted_move
+      puts "Enter which column you'd like to drop your token in Player#{self.player}"
+      move = gets.chomp.to_i - 1
+      until move >= 0 && move <= 5
+        puts 'Please enter an integer between 1 and 6!'
+        move = gets.chomp.to_i - 1
+      end
+      if board.spaces[move].length < 6
+        board.spaces[move].push(self.symbol)
+        accepted_move = true
+      else
+        puts 'It looks like that column is full!'
+      end
+    end
+    move
   end
 end
 
@@ -21,26 +34,124 @@ class Board
   attr_accessor :spaces
 
   def initialize
-    @spaces = Array.new(6, [])
+    @spaces = [[], [], [], [], [], []]
   end
 
   def display_board
-    
+    5.downto(0) do |row|
+      print '|'
+      0.upto(5) do |column|
+        token = spaces[column].length >= (row + 1) ? spaces[column][row] : '_'
+        print "_#{token}_|"
+      end
+      puts
+    end
+  end
+
+  def display_labeled_board
+    puts 'The columns you can place your tokens in are labeled 1-6 on the board below'
+    display_board
+    puts '  1   2   3   4   5   6'
   end
 
   def vertical?(player, move)
+    row = spaces[move].length - 1
+    1.upto(3) do
+      row -= 1
+      return false if row < 0
 
+      return false unless spaces[move][row] == player.symbol
+
+    end
+    true
   end
 
   def horizontal?(player, move)
-
+    row = spaces[move].length - 1
+    directions = [1, -1]
+    0.upto(1) do |direction|
+      column = move
+      win = true
+      1.upto(3) do
+        column += directions[direction]
+        if (column > 5 || column < 0)
+          win = false 
+          break
+        end
+        win = false unless spaces[column][row] == player.symbol
+      end
+      return true if win
+    end
+    false
   end
 
   def diagonal?(player, move)
+    directions = [[1, 1], [1, -1], [-1, 1], [-1, -1]]
+    0.upto(3) do |direction|
+      row = spaces[move].length - 1
+      column = move
+      win = true
+      1.upto(3) do
+        row += directions[direction][0]
+        column += directions[direction][1]
+        if (row > 5 || row < 0) || (column > 5 || column < 0)
+          win = false
+          break
+        end
+        win = false unless spaces[column][row] == player.symbol
+      end
+      return true if win
+    end
+    false
+  end
 
+  def board_full?
+    spaces.each { |index| return false if index.length < 6 }
+    true
   end
 
   def winner?(player, move)
     diagonal?(player, move) || horizontal?(player, move) || vertical?(player, move)
   end
 end
+
+class Gameplay
+
+  def create_players
+    puts "Hello players! Welcome to Connect Four!\nPlayer1 would you like to be X or O?"
+    player1 = gets.chomp.upcase
+    until %[X O].include?(player1)
+      puts 'Please choose either X or O'
+      player1 = gets.chomp.upcase
+    end
+    player2 = player1 == 'X' ? 'O' : 'X'
+    puts "Player2 looks like that makes you #{player2}"
+    [Player.new(1, player1), Player.new(2, player2)]
+  end
+
+  def play_again?(winner)
+    winner == 'cats' ? puts('Cats game!') : puts("Looks like Player#{winner.player} won!")
+    puts 'Enter y if you would like to start another game!'
+    response = gets.chomp.downcase
+    return true if response == 'y'
+
+    false
+  end
+
+  def start_game
+    player1, player2 = create_players
+    board, winner, player = Board.new, false, player2
+    board.display_labeled_board
+    until winner
+      player = player == player1 ? player2 : player1
+      move = player.make_a_move(board)
+      board.display_board
+      winner = 'cats' if board.board_full?
+      winner = player if board.winner?(player, move)
+    end
+    play_again?(winner) ? start_game : puts('See ya later!')
+  end
+end
+
+game = Gameplay.new
+game.start_game
--- connect_four/spec/connect_four_spec.rb
+++ connect_four/spec/connect_four_spec.rb
@@ -13,11 +13,25 @@ RSpec.describe Player do
     before do
       allow($stdout).to receive(:puts)
       io.puts '3'
+      io.puts '3'
+      io.puts '4'
       io.rewind
     end
 
     it 'gets user input and pushes it to proper index in board array' do
       $stdin = io
+      expect { player.make_a_move(board) }.to change{ board.spaces[2] }.from([]).to(['X'])
+      $stdin = STDIN
+    end
+
+    it 'prompts for user input until acceptable input is entered' do
+      $stdin = io
+      board.spaces[2].push('X')
+      board.spaces[2].push('X')
+      board.spaces[2].push('X')
+      board.spaces[2].push('X')
+      board.spaces[2].push('X')
+      board.spaces[2].push('X')
       expect { player.make_a_move(board) }.to change{ board.spaces[3] }.from([]).to(['X'])
       $stdin = STDIN
     end
@@ -32,15 +46,16 @@ RSpec.describe Board do
 
     context 'checking for a win vertically (within an array)' do
       
-      xit 'returns true when player makes a winning move' do
+      it 'returns true when player makes a winning move' do
         board.spaces[0].push('X')
         board.spaces[0].push('X')
         board.spaces[0].push('X')
-        expect(board.vertical(player, 0)).to eql(true)
+        board.spaces[0].push('X')
+        expect(board.vertical?(player, 0)).to eql(true)
       end
 
-      xit 'returns false when players move does not cause them to win' do
-        expect(board.vertical(player, 3)).to eql(false)
+      it 'returns false when players move does not cause them to win' do
+        expect(board.vertical?(player, 3)).to eql(false)
       end
     end
   end
@@ -52,15 +67,17 @@ RSpec.describe Board do
 
     context 'checking for a win horizontally (across arrays)' do
       
-      xit 'returns true when player makes a winning move' do
+      it 'returns true when player makes a winning move' do
         board.spaces[0].push('X')
         board.spaces[1].push('X')
         board.spaces[2].push('X')
-        expect(board.horizontal(player, 3)).to eql(true)
+        board.spaces[3].push('X')
+        expect(board.horizontal?(player, 3)).to eql(true)
       end
 
-      xit 'returns false when players move does not cause them to win' do
-        expect(board.horizontal(player, 0)).to eql(false)
+      it 'returns false when players move does not cause them to win' do
+        board.spaces[0].push('X')
+        expect(board.horizontal?(player, 0)).to eql(false)
       end
     end
   end
@@ -72,21 +89,14 @@ RSpec.describe Board do
 
     context 'checking for a win diagonally' do
       
-      xit 'returns true when player makes a winning move' do
-        board.spaces[0].push('X')
-        board.spaces[1].push('O')
-        board.spaces[1].push('X')
-        board.spaces[2].push('O')
-        board.spaces[2].push('O')
-        board.spaces[2].push('X')
-        board.spaces[3].push('O')
-        board.spaces[3].push('O')
-        board.spaces[3].push('O')
-        expect(board.diagonal(player, 3)).to eql(true)
+      it 'returns true when player makes a winning move' do
+        board.spaces = [['X'], ['O','X'], ['O', 'O', 'X'], ['O', 'O', 'O', 'X'], [], []]
+        expect(board.diagonal?(player, 3)).to eql(true)
       end
 
-      xit 'returns false when player does not make a winning move' do
-        expect(board.diagonal(player, 4)).to eql(false)
+      it 'returns false when player does not make a winning move' do
+        board.spaces[4].push('X')
+        expect(board.diagonal?(player, 4)).to eql(false)
       end
     end
   end
@@ -97,30 +107,109 @@ RSpec.describe Board do
 
     context 'verifying that the gameboard prints out correctly' do
       
-      xit 'is capable of printing out an empty gameboard' do
-        expect{ board.display_board }.to output(" ___________________
-|___|___|___|___|___|
-|___|___|___|___|___|
-|___|___|___|___|___|
-|___|___|___|___|___|
-|___|___|___|___|___|
-|___|___|___|___|___|").to_stdout
+      it 'is capable of printing out an empty gameboard' do
+        expect{ board.display_board }.to output("|___|___|___|___|___|___|
+|___|___|___|___|___|___|
+|___|___|___|___|___|___|
+|___|___|___|___|___|___|
+|___|___|___|___|___|___|
+|___|___|___|___|___|___|\n").to_stdout
       end
 
-      xit 'is capable of printing out a gameboard with spaces taken' do
+      it 'is capable of printing out a gameboard with spaces taken' do
         board.spaces[0].push('X')
         board.spaces[1].push('X')
         board.spaces[2].push('X')
         board.spaces[3].push('O')
         board.spaces[3].push('O')
         board.spaces[3].push('O')
-        expect{ board.display_board }.to output(" ___________________
-|___|___|___|___|___|
-|___|___|___|___|___|
-|___|___|___|___|___|
-|___|___|___|_O_|___|
-|___|___|___|_O_|___|
-|_X_|_X_|_X_|_O_|___|").to_stdout
+        expect{ board.display_board }.to output("|___|___|___|___|___|___|
+|___|___|___|___|___|___|
+|___|___|___|___|___|___|
+|___|___|___|_O_|___|___|
+|___|___|___|_O_|___|___|
+|_X_|_X_|_X_|_O_|___|___|\n").to_stdout
+      end
+    end
+  end
+
+  describe '#board_full?' do
+
+    let(:board) { Board.new }
+
+    context 'checks if board is full which would result in a Cats Game' do
+
+      it 'returns false if board is not full' do
+        expect(board.board_full?).to eql(false)
+      end
+
+      it 'returns true if board is completely full' do
+        board.spaces = [['X', 'X', 'X', 'X', 'X', 'X'],
+                        ['X', 'X', 'X', 'X', 'X', 'X'],
+                        ['X', 'X', 'X', 'X', 'X', 'X'],
+                        ['X', 'X', 'X', 'X', 'X', 'X'],
+                        ['X', 'X', 'X', 'X', 'X', 'X'],
+                        ['X', 'X', 'X', 'X', 'X', 'X']]
+        expect(board.board_full?).to eql(true)
+      end
+    end
+  end
+end
+
+RSpec.describe Gameplay do
+  describe '#create_players' do
+
+    let(:io) { StringIO.new }
+    let(:gameplay) { Gameplay.new }
+
+    before do
+      allow($stdout).to receive(:puts)
+      io.puts 'X'
+      io.rewind 
+    end
+
+    it 'returns 2 instances of Player class' do
+      $stdin = io
+      expected_array = [instance_of(Player), instance_of(Player)]
+      expect(gameplay.create_players).to match_array(expected_array)
+      $stdin = STDIN
+    end
+
+    it 'returns player classes with expected attributes' do
+      $stdin = io
+      players = gameplay.create_players
+      expect(players[0]).to have_attributes(:player => 1, :symbol => 'X')
+      $stdin = STDIN
+    end
+  end
+
+  describe '#play_again?' do
+
+    let(:gameplay) { Gameplay.new }
+    let(:io) { StringIO.new }
+    let(:io2) { StringIO.new }
+    let(:player) { Player.new(1, 'X')}
+
+    before do
+      allow($stdout).to receive(:puts)
+      io.puts 'y'
+      io2.puts 'n'
+      io.rewind
+      io2.rewind
+    end
+
+    context 'returns true or false to decide whether or not to start another game' do
+
+      it 'returns true when player wants to play again' do
+        $stdin = io
+        expect(gameplay.play_again?(player)).to eql(true)
+        $stdin = STDIN
+      end
+
+      it 'returns false when player does not want to play again' do
+        $stdin = io2
+        expect(gameplay.play_again?(player)).to eql(false)
+        $stdin = STDIN
       end
     end
   end
